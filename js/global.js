fd1d244f5db5e7280f589d98931bf7935f9d8639
Fenix <ifeng.name@gmail.com>
2013-01-26 12:18:09 +0800

    fdasfdas

--- js/global.js
+++ js/global.js
@@ -24,35 +24,19 @@ $('#mod-course').find('.cdetail-data-list li span').live('click',function(event)
 				  $('#mod-course').find('.cdetail-data-list li[data-pid=' + json.pid + ']').remove();
 				}
 			  }catch(e) {
-				//alert('删除失败，请稍后重试');
-				insetAlert('notice','删除失败，请稍后重试');
+				alert('删除失败，请稍后重试');
+				insetAlert('notice','删除失败，请稍后重试',false,false,'');
 			  }
 		}
 		});
 	})
 });
 
-//回答追问
-$('.ufeeds-answer').live('click',function(){
-  questionAction($(this));
-});
-$('.ufeeds-addask').live('click',function(){
-  questionAction($(this));
-});
-function questionAction(obj){
-	obj.append('<span class="question-arrow"></span>');
-	obj.parents('.ucenter-feeds-wrap').siblings('.comment-box').show();
-}
-//回答追问展开收起
-$('.comment-fold').live('click',function(){
-	$(this).parents('.comment-box').slideUp('fast');
-	$(this).parents('.comment-box').siblings('.ucenter-feeds-wrap').find('.question-arrow').remove();
-});
 //进入课程
 $('.ucenter-content-course dl:not(.ucontent-add)').hover(function(){
   //e.stopPropagation();
   var _href = $(this).find('dt a').attr('href');
-  $(this).find('dt').append('<a class="ucourse-hover"></a><a class="ucourse-hover-span" target="_blank">进入课程</a>');
+  $(this).find('dt').append('<a class="ucourse-hover"></a><a class="ucourse-hover-span">进入课程<i class="icon-arrow-white"></i></a>');
   $(this).find('dt .ucourse-hover').animate({opacity:'0.3'},100);
   $(this).find('dt .ucourse-hover-span').animate({left:'0'},100);
 
@@ -64,7 +48,8 @@ $('.ucenter-content-course dl:not(.ucontent-add)').hover(function(){
 });
 //师生动态展开收起
 $('.ucenter-feeds-unfold').live('click',function(){
-  	var _height = $(this).siblings().find('.ucenter-feeds-text').height()+$(this).siblings().find('.ucenter-feeds-img').height();
+  	//var _height = $(this).siblings().find('.ucenter-feeds-text').height()+$(this).siblings().find('.ucenter-feeds-img').height();
+  	var _height = $(this).siblings('.ufeeds-detail-foldarea').height();
 	$(this).siblings('.ufeeds-detail-foldarea').animate({'height':_height,'overflow':'visible'},300);
 	$(this).removeClass('ucenter-feeds-unfold').addClass('ucenter-feeds-fold').text('收起');
 });
@@ -78,12 +63,17 @@ $('.no-data .accordion-toggle').css('cursor','default');
 $('.no-data .accordion-toggle').find('.arrow-gray-right').remove();
 
 //课程大纲资料翻页
-var dataLen = $('.cdetail-data-list').length;
-var dataPage = 1;
-$(".dthumb li").click(function(){
-  dataPage =   $(".dthumb li").index(this);
-  DataScroll(dataPage);
-});	
+//var dataLen = $('.cdetail-data-list').length;
+//var dataPage = 1;
+//$(".dthumb li").click(function(){
+//  dataPage =   $(".dthumb li").index(this);
+//  DataScroll(dataPage);
+//});	
+
+$('.dthumb').live('click',function(e){
+	//e.preventDefault();
+	//e.stopPropagation();
+});
 function DataScroll(page){
   $('.cdetail-data-list').eq(page).fadeIn().siblings().fadeOut();
   //$('.pic_list li').eq(page).fadeOut();
@@ -126,15 +116,87 @@ $('#import-outline li').live('click',function(){
 })
 
 $('#modal-set-course .btn-group .btn').click(function(e){ e.stopPropagation(); });
+//编辑大纲
+var outline = {
+	chapterHtml : function(cid){
+		  return '<div class="item"><h6 data-cid="'+(cid+1)+'" data-level="1"><span class="item-num">第'+(cid+1)+'章</span><input class="item-input" id="" name="" type="text" placeholder="输入本章名" /></h6><div class="create" id="section-create" title="创建新的节"><i class="icon-setcourse-plus"></i>创建新的节</div></div>';
+	},
+	sectionHtml : function(sid){
+		return '<div class="item"><h6 data-sid="'+(sid+1)+'" data-level="2"><span class="item-num">第'+(sid+1)+'节</span><input class="item-input" id="" name="" type="text" placeholder="输入本节名" /></h6><div class="create" id="sub-section-create" title="创建新的小节"><i class="icon-setcourse-plus"></i>创建新的小节</div></div>';			  
+	},
+	subSectionHtml : function(ssid){
+		return '<div class="item"><h6 data-ssid="'+(ssid+1)+'" data-level="3"><span class="item-num">第'+(ssid+1)+'小节</span><input class="item-input" id="" name="" type="text" placeholder="输入本小节名" /></h6></div>';
+	},
+	createJson : function(level,itemContent){
+		 switch(level){
+			 case '1' : 
+				 chapterObj.name = itemContent;
+				 outlineData.chapter.push(chapterObj);
+				 //console.log(outlineData);
+			 break;
+			 case '2' :
+				 sectionObj.name = itemContent;
+				 chapterObj.section.push(sectionObj);
+				 //console.log(outlineData);
+			 break;
+			 case '3' :
+				 sectionObj.sub.push(itemContent);
+				 console.log(outlineData);
+			 break;
+		 }
+	},
+	save : function(){
+		//完成按钮触发，ajax向后台发送数据outlineData
+		$.ajax({
+			type : 'POST',
+			url : '',
+			success : function(){
+				//console.log(itemContent);		
+			}
+		});	
+	},
+	init : function() {
+		outlineData = { chapter : [] };
+		chapterObj = { name : '', section : [] };
+		sectionObj = { name : '', sub : [] };
+		//章节id
+		cid = 0;
+		sid = 0;
+		ssid = 0;
+	}
+}
+
+outline.init();
+
+$('#edit-outline').delegate('#chapter-create','click',function(){
+		$(this).before(outline.chapterHtml(cid));
+		cid++;
+		sid =0;
+		ssid =0;
+});
+
+$('#edit-outline').delegate('#section-create','click',function(){
+		$(this).before(outline.sectionHtml(sid));
+		sid++;
+		ssid =0;
+});
+$('#edit-outline').delegate('#sub-section-create','click',function(){
+		$(this).before(outline.subSectionHtml(ssid));
+		ssid++;
+});
+$('#edit-outline').delegate('.item-input','blur',function(){
+	//input值
+	var itemContent = $(this).val();
+	//层级
+	var level = $(this).parent('h6').attr('data-level');
+	outline.createJson(level,itemContent);
+	$(this).replaceWith('<p>'+itemContent+'</p>');
+});
 
 /*$('#select-grade').change(function(){
   /*ajax获取选中年级下的班级list
-	   
-   
-   
   $('#choose-class .controls').append('xxx');
 });*/
-
 
 //select美化
 if($('select').length != 0){
@@ -143,7 +205,7 @@ if($('select').length != 0){
   $('select.select-small').next('.dropselectbox').find('ul').css({'width':'125px'});
 }
 //课程打分
-doGrade($('.course-rating-ul'));
+//doGrade($('.course-rating-ul'));
 //----------------------------------------
 });
 var libedu_resource_manager = 
@@ -185,7 +247,10 @@ var libedu_resource_manager =
 	},
 }
 function PreviewData(){
+	//alert('ajax加载资料预览数据');
 
+    //return '<object width="100%" height="100%"><param name="movie" value="http://www.tudou.com/v/qCQKhudHLMo"><param name="allowFullScreen" value="true"><param name="allowscriptaccess" value="always"> <param name="wmode" value="opaque"><embed src="http://www.tudou.com/v/qCQKhudHLMo" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" wmode="opaque" width="100%" height="85%"></object>';
+   return '<div class="sidebar-preview-data-text"><p>原因很清楚，当我们的调控都是以短期行为为目标的时候，通常会发生后遗症，一定是错的。中国最重要的是政府任届期，他会要求在短期市场中出现一个符合他心中要求的结果，他不会去管后期或者我们叫做长效机制而产生的结果。如果用市场经济的办法调控市场的话，可能对长远来说会让市场机制更加健全，让这只看不见的手发挥巨大作用。而我们的调控基本上是破坏市场经济机理，就是把市场经济的机制破坏的越严重越好，这样就可以按照他们的要求，今天这样，明天那样。就是我想让这个猪肉价格高点就高点，想让猪肉价格低点就低点。计划经济时代，毛主席有一个“论十大关系”，其中讲了一个总供给和总需求之间的关系。但是，到了市场经济，我突然发现没有这个概念了，不管总需求和总供给之间有什么差别，反正就是我说了算，我想让它高它就高点，我想让它低就是低点。从从从从从从从从从从从从从从从2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的2003年以后大部分是打压的。结果2008年经济危机之后我们出现了鼓励或者是振兴，弄把它弄上来了。从往下调到往上调，这几次都可以看到，2007年是往下调，是“两防”。</p></div>';
 }
 function SetCourseScroll(obj,offset,curStep,pos){
   obj.live('click',function(){
@@ -224,39 +289,4 @@ function insetAlert(alert_type,alert_con_msg,is_aside){
   alertHtml += '</p> </div> </div>';
   $('body').append(alertHtml);
   $('#alert').modal('show');
-}
-function doGrade(obj){
-  	var index,
-		differ,
-		score;
-	obj.each(function(){
-		obj.find('li').on({
-			mouseover: function (){
-				$(this).prevAll().removeClass().addClass('rating-one');
-				index = $(this).index()+1;
-			},
-			mousemove: function (e){
-				differ = parseFloat(e.pageX - $(e.target).offset().left);
-				if(differ < 9){
-					$(this).removeClass().addClass('rating-half');
-					score = (index-1)*2+1;
-				} else {
-					$(this).removeClass().addClass('rating-one');
-		  			score = index*2;
-				}
-				$(this).attr({'title' : score+'分' , 'data-score' : score});
-			},
-			click : function(){
-				//这里是打分ajax请求
-				//打分成功后解绑所有的事件并且移除course-rating-ul
-				$(this).unbind();
-				$(this).siblings().unbind();
-				$(this).parent('ul').unbind();
-				$(this).parent('ul').removeClass('course-rating-ul');
-
-				alert($(this).attr('data-score'));	
-			}
-		});
-		obj.on('mouseout',function(){ $(this).find('li').removeClass(); });
-	});
 }
---
+++ test.js
@@ -0,0 +1,20 @@
+﻿//function A(a, b, c) {
+//  var ar = [a, b, c];
+//  return function B(i) {
+//    return ar[i];
+//  };
+//}
+// 
+//var b = A('Here', 'I', 'am');
+//console.log( b(1) );
+//-------------------------------
+//(function(){
+//	var s = 'hello';
+//	s.substring(1, 4);
+//	//console.log(s);
+//})();
+//----------------------
+//(function(){
+	function Sum(a, b) { return a + b; }
+	var inc = Sum.bind(null, 1); // 将形参a绑定为1,this绑定为null
+//})();
